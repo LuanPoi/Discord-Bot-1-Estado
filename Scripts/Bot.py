eac14582d59fb9e3882c5add3cdefbeea7225a9c
Luan Poi <luan_poi@hotmail.com>
2020-01-21 23:37:19 -0300

    Inicio do projeto, estrutura base criada.
    Criada a lógica base do bot, de modo minimamente funcional.

---
+++ Scripts/Bot.py
@@ -0,0 +1,62 @@
+import discord
+import yaml
+
+_config = yaml.safe_load(open('..\config.yaml', 'r'))
+
+estado = discord.Client()
+
+@estado.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(estado))
+
+@estado.event
+async def on_message(message):
+    if message.author == estado.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        return
+
+    if (message.author.id != ((_config['guardaSovietico'])['id'])) & (message.author.id != ((_config['oficialSovietico'])['id'])) & (message.content.endswith('Eu trouxe vodka')):
+        user = message.author
+        role = discord.utils.get(user.guild.roles, name="Autorização de entrada")
+        await user.add_roles(role)
+        return
+
+@estado.event
+async def on_voice_state_update(member, before, after):
+    if(before.channel != None):
+        if(after.channel != None):
+            return
+        else:
+            if(before.channel.id == (((_config['canais'])['deVoz'])['Moscou'])):
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.add_roles(role)
+                role = discord.utils.get(member.guild.roles, name='Moscou')
+                await member.remove_roles(role)
+                return
+            elif(before.channel.id == (((_config['canais'])['deVoz'])['Stalingrado'])):
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.add_roles(role)
+                role = discord.utils.get(member.guild.roles, name='Stalingrado')
+                await member.remove_roles(role)
+                return
+    else:
+        if(after.channel != None):
+            if(after.channel.id == (((_config['canais'])['deVoz'])['Moscou'])):
+                role = discord.utils.get(member.guild.roles, name='Moscou')
+                await member.add_roles(role)
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.remove_roles(role)
+                return
+            elif(after.channel.id == (((_config['canais'])['deVoz'])['Stalingrado'])):
+                role = discord.utils.get(member.guild.roles, name='Stalingrado')
+                await member.add_roles(role)
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.remove_roles(role)
+                return
+        else:
+            return
+
+estado.run(_config['discordBotKey'])
