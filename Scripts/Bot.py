2d265fa2496d694670babf980bf4a75c26418248
LuanPoi - Ubuntu <luan_poi@hotmail.com>
2020-03-30 16:22:31 -0300

    Corrigindo bug de entrar na sala mutado

--- Scripts/Bot.py
+++ Scripts/Bot.py
@@ -1,7 +1,7 @@
 import discord
 import yaml
 
-_config = yaml.safe_load(open('..\config.yaml', 'r'))
+_config = yaml.safe_load(open("../config.yaml", 'r'))
 
 estado = discord.Client()
 
@@ -18,7 +18,7 @@ async def on_message(message):
         await message.channel.send('Hello!')
         return
 
-    if (message.author.id != ((_config['guardaSovietico'])['id'])) & (message.author.id != ((_config['oficialSovietico'])['id'])) & (message.content.endswith('Eu trouxe vodka')):
+    if (message.author.id != ((_config['guardaSovietico'][0])['id'])) & (message.author.id != ((_config['oficialSovietico'][0])['id'])) & (message.content.endswith('Eu trouxe vodka')):
         user = message.author
         role = discord.utils.get(user.guild.roles, name="Autorização de entrada")
         await user.add_roles(role)
@@ -28,34 +28,63 @@ async def on_message(message):
 async def on_voice_state_update(member, before, after):
     if(before.channel != None):
         if(after.channel != None):
-            return
+            if (after.channel.id == (((_config['canais'])['deVoz'])['Moscou'])):
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.remove_roles(role)
+                
+                role = discord.utils.get(member.guild.roles, name='Moscou')
+                await member.add_roles(role)
+                await member.edit(mute = False)
+                return
+
+            elif (after.channel.id == (((_config['canais'])['deVoz'])['Stalingrado'])):
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.remove_roles(role)
+                
+                role = discord.utils.get(member.guild.roles, name='Stalingrado')
+                await member.add_roles(role)
+                await member.edit(mute = False)
+                return
+
+            else:
+                return
+
         else:
             if(before.channel.id == (((_config['canais'])['deVoz'])['Moscou'])):
-                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
-                await member.add_roles(role)
                 role = discord.utils.get(member.guild.roles, name='Moscou')
                 await member.remove_roles(role)
-                return
-            elif(before.channel.id == (((_config['canais'])['deVoz'])['Stalingrado'])):
+
                 role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
                 await member.add_roles(role)
+                return
+
+            elif(before.channel.id == (((_config['canais'])['deVoz'])['Stalingrado'])):
                 role = discord.utils.get(member.guild.roles, name='Stalingrado')
                 await member.remove_roles(role)
+
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.add_roles(role)
                 return
+
     else:
         if(after.channel != None):
             if(after.channel.id == (((_config['canais'])['deVoz'])['Moscou'])):
+                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
+                await member.remove_roles(role)
+
                 role = discord.utils.get(member.guild.roles, name='Moscou')
                 await member.add_roles(role)
+                await member.edit(mute = False)
+                return
+
+            elif(after.channel.id == (((_config['canais'])['deVoz'])['Stalingrado'])):
                 role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
                 await member.remove_roles(role)
-                return
-            elif(after.channel.id == (((_config['canais'])['deVoz'])['Stalingrado'])):
+                
                 role = discord.utils.get(member.guild.roles, name='Stalingrado')
                 await member.add_roles(role)
-                role = discord.utils.get(member.guild.roles, name="Autorização de entrada")
-                await member.remove_roles(role)
                 return
+
         else:
             return
 
